c13929f28b779b19db4deff66f24d1f38aef3669
edutech4 <edutech4logic@gmail.com>
2021-08-17 20:03:46 +0100

    Added code for positioning the names based on the length of the names, created multiple pdf files and also merged the pdf files generated and also n added  the code for deleting the files generated

--- certificate_creator.py
+++ certificate_creator.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
+from PyPDF2 import PdfFileMerger, PdfFileReader
+import os
+import shutil
+
 import qrcode
 
 
@@ -11,27 +15,54 @@ import qrcode
 
 #--------------------------------------------------------------------------------------------------------------------
 #-------------THIS SECTION OF CODE ATTACHES NAMES GOTTEN FROM EXCEL TO THE TEMPLATE OF THE CERTIFICATE---------------
-data = pd.read_excel(r'C:/Users/MO_WIZZ/Documents/Python_Project/Certificate_creator/Certname.xlsx')
-name_list = data["NAME"].tolist()
-name_date = data["DATE"].tolist()
-qr_code_data = data["CERTNO"].tolist()
-
-print(name_date[0])
-print(qr_code_data)
-image = qrcode.make(qr_code_data[0])
-image.save("qrcode.jpg")
-
+data = pd.read_excel(r'C:/Users/MO_WIZZ/Documents/Python_Project/Certificate_creator/certname_giz.xlsx')
+name_list = data["CERTNAME"].tolist()
+# name_date = data["DATE"].tolist()
+# qr_code_data = data["CERTNO"].tolist()
+# print(name_list)
+# print(name_date[0])
+# print(qr_code_data)
+# image = qrcode.make(qr_code_data[0])
+# image.save("qrcode.jpg")
+file_number = 0
+number_of_files = 0
 for i in name_list:
     i_edited = i.replace(" ","  ")  #Adding more spaces between surname and firstname
     # print(i)
+    # i_edited = name_list[5]
+    # print(len(name_list[5]))
     im = Image.open(r'C:/Users/MO_WIZZ/Documents/Python_Project/Certificate_creator/GIZ_CERT_template.jpg')
     d = ImageDraw.Draw(im)
     # location = (1150, 1070)  #x, y Robotics centre certificate cordinate
-    location = (1146, 1254)  # x, y
+    if len(i_edited) < 18:
+        location = (1350, 1254)  # x, y
+    elif len(i_edited) > 21:
+        location = (1000, 1254)  # x, y
+    else:
+        location = (1230, 1254)  # x, y
     text_color = (0, 137, 209)
     font = ImageFont.truetype("ITCBLKAD.ttf", 150)
     d.text(location, i_edited, fill =29, font=font)
-    im.save("certificate_" + i + ".pdf")  #Saves as pdf
+    file_number = file_number + 1
+    number_of_files = file_number
+    # im.save("certificate_" + i + ".pdf")  #Saves as pdf
+    im.save(str(file_number) + ".pdf")  # Saves as pdf
+
+##------------------THIS SECTION HELPS TO MERGE MULTIPLE PDF FILES-------------------------------------------------------
+file_number = 0
+mergedObject = PdfFileMerger()
+for fileNumber in range(number_of_files):
+    file_number = file_number + 1
+    mergedObject.append(PdfFileReader(str(file_number) + '.pdf', 'rb'))
+
+# Write all the files into a file which is named as shown below
+mergedObject.write("mergedfilesoutput.pdf")
+
+
+#####--------------------THIS SECTION HELPS TO DELETE MULTIPLE FILES THAT WAS CREATED BEFORE THIS SECTION-------------------
+# for filename in range(1, 51):
+#     if os.path.exists(str(filename)+'.pdf'):
+#         os.remove(str(filename)+'.pdf')
 
 #--------------------------------------------------------------------------------------------------------------------
 #-------------THIS SECTION OF CODE ATTACHES QR CODE TO THE TEMPLATE OF THE CERTIFICATE-------------------------------
