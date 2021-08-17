ec72a0abdd04f93e66157b0e04b1138b1bf92bab
edutech4 <edutech4logic@gmail.com>
2021-08-17 18:13:47 +0100

    Creating the certificate for the giz students trained on robotics and ai

--- certificate_creator.py
+++ certificate_creator.py
@@ -9,23 +9,29 @@ import qrcode
 # image.save("qrcode.jpg")
 
 
-
 #--------------------------------------------------------------------------------------------------------------------
 #-------------THIS SECTION OF CODE ATTACHES NAMES GOTTEN FROM EXCEL TO THE TEMPLATE OF THE CERTIFICATE---------------
-data = pd.read_excel(r'C:/Users/MO_WIZZ/Documents/Python_Project/Certificate_creator/test.xlsx')
+data = pd.read_excel(r'C:/Users/MO_WIZZ/Documents/Python_Project/Certificate_creator/Certname.xlsx')
 name_list = data["NAME"].tolist()
-# print(name_list)
+name_date = data["DATE"].tolist()
+qr_code_data = data["CERTNO"].tolist()
+
+print(name_date[0])
+print(qr_code_data)
+image = qrcode.make(qr_code_data[0])
+image.save("qrcode.jpg")
+
 for i in name_list:
     i_edited = i.replace(" ","  ")  #Adding more spaces between surname and firstname
     # print(i)
-    im = Image.open(r'C:/Users/MO_WIZZ/Documents/Python_Project/Certificate_creator/CERTIFICATE2.jpg')
+    im = Image.open(r'C:/Users/MO_WIZZ/Documents/Python_Project/Certificate_creator/GIZ_CERT_template.jpg')
     d = ImageDraw.Draw(im)
-    location = (1150, 1070)  #x, y
+    # location = (1150, 1070)  #x, y Robotics centre certificate cordinate
+    location = (1146, 1254)  # x, y
     text_color = (0, 137, 209)
     font = ImageFont.truetype("ITCBLKAD.ttf", 150)
     d.text(location, i_edited, fill =29, font=font)
     im.save("certificate_" + i + ".pdf")  #Saves as pdf
-
 
 #--------------------------------------------------------------------------------------------------------------------
 #-------------THIS SECTION OF CODE ATTACHES QR CODE TO THE TEMPLATE OF THE CERTIFICATE-------------------------------
